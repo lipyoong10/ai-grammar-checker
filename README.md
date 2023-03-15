7f568f7fb9f5256041684180079a3326c8beecf3
Lippy Kong <lippy.kong@ecloudvalley.com>
2023-03-15 16:23:29 +0800

    This is to push latest code into repo (first push)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ai-grammar-checker
 A simple web application that corrects your grammar with GPT-3
+
+To run this application:
+1. Make sure to install the latest Python on your computer (https://www.python.org/downloads/)
+2. Open "aigrammarchecker.py", and edit the following:
+    a. openai.api_key - Get your key by creating an account and generating an API key (https://platform.openai.com/account/api-keys)
+3. Run the script, and access it via localhost:7860
+
+Do note that the default username and password is "username":"password". Do update this! 
---
+++ aigrammarchecker.py
@@ -0,0 +1,29 @@
+#Import open AI OS and System Modules
+
+import openai
+import gradio as gr
+
+openai.api_key = 'platform openai api_key'
+
+messages = [
+    {"role": "system", "content": "There are only 2 outputs: If no error, say 'Grammatically correct'. Else, reply in full with the corrected grammar with no explanation"},
+]
+
+def chatbot(input):
+    if input:
+        messages.append({"role": "user", "content": "Please correct my grammar \n\n"+input})
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        return reply
+
+inputs = gr.components.Textbox(lines=30, label="Paste your sentence here (4000 character limit)") #Limit of 4000 chars
+outputs = gr.components.Textbox(lines=30, label="AI grammar corrected output")
+
+demo = gr.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title="AI Grammar Checker",
+             description="AI-assisted Grammar Correction (Slowness due to smaller server)",
+             theme=gr.themes.Base(), css="footer {visibility: hidden}").launch(server_name="0.0.0.0",auth=("username","password"))
+
